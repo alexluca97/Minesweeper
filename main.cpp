6c4f7fc9a42dde32f8c2083e9c3bf7732766d775
alexluca97 <alexluca97@yahoo.com>
2017-01-08 16:23:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -45,3 +45,18 @@ void bmbselect(int i,int j)
 		if (i > 0 && j > 0 && a[i - 1][j - 1] == -1)a[i][j]++;
 	}
 }
+
+int main(int argc, char* argv[])
+{
+	char red[] = { 0x1b, '[', '1', ';', '3', '1', 'm', 0 };
+	char normal[] = { 0x1b, '[', '0', ';', '3', '9', 'm', 0 };
+	
+	int i, j;
+	int q, r;
+	cout << "DATI N=";
+	cin >> n;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			b[i][j] = 'O';
+}
+
